94e2232db8823fb1d71157f4af1a52f8e04f1e34
Andres Alvarez <72172628+andresalvarezb@users.noreply.github.com>
2024-02-22 09:01:49 -0500

    feat: :sparkles: Create section about popular categories
    Add two libraries, flaticon one and swiper one. Also, we add styles in this section

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,31 @@
 /*=============== IMAGE GALLERY ===============*/
 
 /*=============== SWIPER CATEGORIES ===============*/
+var swiperCategories = new Swiper('.categories__container', {
+    spaceBetween: 20,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    breakpoints: {
+        "@0.00": {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        "@0.75": {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+        "@1.00": {
+            slidesPerView: 4,
+            spaceBetween: 30,
+        },
+        "@1.50": {
+            slidesPerView: 6,
+            spaceBetween: 25,
+        },
+    },
+})
 
 /*=============== SWIPER PRODUCTS ===============*/
 
